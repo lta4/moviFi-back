8637b645e5a4b938ead67154a303937b9e6d2a2e
Andrew <andrew.mk4@yahoo.com>
2021-04-22 14:13:10 -0700

    first back commit

---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const { PORT, NODE_ENV = "development" } = process.env;
+
+const express = require("express");
+const serveIndex = require("serve-index");
+const cors = require("cors");
+
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+
+app.use(morgan("tiny"));
+
+app.use((req, res, next) => {
+    console.log("Time: ", Date.now());
+    next();
+});
+
+app.use("/login", (req, res) => {
+    res.send({
+        token: "test123"
+    });
+});
+
+app.listen(8080, () => console.log("API is running on http://localhost:8080/login"));
+
+app.use("/request-type", (req, res, next) => {
+    console.log("Request type: ", req.method);
+    next();
+});
+
+app.use("/public", express.static("public"));
+app.use("/public", serveIndex("public"));
+
+app.get("/", (req, res) => {
+    res.send("Successful response.");
+});
+
+app.listen(PORT, () => {
+    console.log(`You are listening on port ${PORT}`);
+});
+// app.listen(3000, () => console.log("Example app is listening on port 3000."));
